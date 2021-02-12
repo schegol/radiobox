fdd0cd35ca1aee3a446f0916574b2c9087e8ec93
VladProfitkit <vlad@profitkit.ru>
2021-02-12 19:39:56 +0300

    Верстка футера и хедера (требуется работа над адаптивом главного меню)

--- build/js/script.js
+++ build/js/script.js
@@ -1,3 +1,73 @@
 $(document).ready(function() {
+  const mobileSearchToggle = $('.header__search-block-toggle');
+  const searchBlock = $('.header__search-form');
+  const searchInput = $('.header__search-form-input');
+  const searchSubmit = $('.header__search-form-submit');
 
+  const mobilePhonesToggle = $('.header__call-block-toggle');
+  const phonesBlock = $('.header__call-block');
+  const phoneNumber = $('.header__call-block-phone');
+  const phoneModalCall = $('.header__call-block-order');
+
+  const openSearch = function () {
+    searchBlock.addClass('header__search-form--open');
+  }
+
+  const closeSearch = function () {
+    searchBlock.removeClass('header__search-form--open');
+  }
+
+  const openPhones = function () {
+    phonesBlock.addClass('header__call-block--open');
+  }
+
+  const closePhones = function () {
+    phonesBlock.removeClass('header__call-block--open');
+  }
+
+  mobileSearchToggle.click(function () {
+    if (searchBlock.hasClass('header__search-form--open')) {
+      closeSearch();
+    } else {
+      openSearch();
+    }
+  });
+
+  mobilePhonesToggle.click(function () {
+    if (phonesBlock.hasClass('header__call-block--open')) {
+      closePhones();
+    } else {
+      openPhones();
+    }
+  });
+
+  $(window).mousedown(function (e) {
+    if (!searchInput.is(e.target) && searchInput.has(e.target).length === 0 && !searchSubmit.is(e.target) && searchSubmit.has(e.target).length === 0) {
+        closeSearch();
+    }
+  });
+
+  $(window).mousedown(function (e) {
+    if (!phoneNumber.is(e.target) && phoneNumber.has(e.target).length === 0 && !phoneModalCall.is(e.target) && phoneModalCall.has(e.target).length === 0) {
+        closePhones();
+    }
+  });
+
+  phoneModalCall.click(function () {
+    closePhones();
+  })
+
+  $(document).keydown(function(e) {
+    if (e.keyCode == 27) {
+        closeSearch();
+        closePhones();
+    };
+  });
+
+  $(window).on('resize', function () {
+    if ($(window).width() > 767) {
+      closeSearch();
+      closePhones();
+    }
+  });
 });
