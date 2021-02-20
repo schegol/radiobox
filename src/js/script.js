6c2c9844d8bc52f041dd925186e3015b46077b7c
VladProfitkit <vlad@profitkit.ru>
2021-02-20 18:07:18 +0300

    Работа над внутренними страницами каталога

--- src/js/script.js
+++ src/js/script.js
@@ -93,5 +93,139 @@ $(document).ready(function() {
   });
 
   //custom scrollbar:
-	$('.news-index__news-list').jScrollPane();
+  const newsList = $('.news-index__news-list');
+  if (newsList.length>0) {
+    	newsList.jScrollPane();
+  }
+
+  //wide sliders:
+  const wideSliderBlocks = $('.wide-slider');
+  const sliderFunc = function(sliderBlock) {
+    let slider = sliderBlock.find('.wide-slider__slider');
+    let sliderPrevBtn = sliderBlock.find('.wide-slider__slider-arrow--prev');
+    let sliderNextBtn = sliderBlock.find('.wide-slider__slider-arrow--next');
+    slider.slick({
+      prevArrow: sliderPrevBtn,
+      nextArrow: sliderNextBtn,
+      autoplay: false,
+      adaptiveHeight: true,
+      slidesToShow: 4,
+      slidesToScroll: 1,
+      infinite: false,
+      responsive: [
+          {
+              breakpoint: 1199,
+              settings: {
+                  slidesToShow: 3
+              }
+          },
+          {
+              breakpoint: 767,
+              settings: {
+                  slidesToShow: 2
+              }
+          },
+          {
+              breakpoint: 575,
+              settings: {
+                  slidesToShow: 1
+              }
+          }
+      ]
+    });
+  }
+
+  wideSliderBlocks.each(function () {
+    sliderFunc($(this));
+  });
+
+  //Кол-во товара:
+  const quantityBlock = $('.element__quantity-block');
+  const quantityInput = quantityBlock.find('.element__quantity-input');
+  const quantityBtnPlus = quantityBlock.find('.element__quantity-btn--more');
+  const quantityBtnMinus = quantityBlock.find('.element__quantity-btn--less');
+  const quantityMore = function () {
+    let quantity = quantityInput.val();
+    quantity++;
+    quantityInput.val(quantity);
+  };
+  const quantityLess = function () {
+    let quantity = quantityInput.val();
+    if (parseInt(quantity) > 1) {
+      quantity--;
+    } else {
+      quantity = 1;
+    }
+    quantityInput.val(quantity);
+  };
+
+  quantityBtnPlus.click(function () {
+    quantityMore();
+  });
+  quantityBtnMinus.click(function () {
+    quantityLess();
+  });
+
+  //tabs:
+  const tabLinks = $('.tabs__link');
+  let activeTabLink = $('.tabs__link--active');
+  let activeHref = activeTabLink.data('tab');
+  let allTabs = $('.tabs__single-tab');
+
+  if(allTabs.length > 0) {
+    allTabs.each(function () {
+      if ($(this).data('tab') !== activeHref) {
+        $(this).hide();
+      }
+    });
+
+    tabLinks.click(function (e) {
+      e.preventDefault();
+      if (!$(this).hasClass('tabs__link--active')) {
+        let tabs = $(this).parents('.tabs').find('.tabs__single-tab');
+        let href = $(this).data('tab');
+
+        tabLinks.removeClass('tabs__link--active');
+        $(this).addClass('tabs__link--active');
+        tabs.each(function () {
+          if ($(this).data('tab') !== href) {
+            $(this).hide();
+          } else {
+            $(this).show();
+          }
+        });
+      }
+    });
+  }
+
+  //product sliders:
+  const detailSlider = $('.detail-slider');
+
+  if (detailSlider.length > 0) {
+    const bigSlider = detailSlider.find('.detail-slider__big');
+    const smallSlider = detailSlider.find('.detail-slider__small');
+    const sliderPrev = detailSlider.find('.detail-slider__big-arrow--prev');
+    const sliderNext = detailSlider.find('.detail-slider__big-arrow--next');
+
+    bigSlider.slick({
+      asNavFor: smallSlider,
+      prevArrow: sliderPrev,
+      nextArrow: sliderNext,
+      autoplay: false,
+      adaptiveHeight: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      infinite: false,
+    });
+    
+    smallSlider.slick({
+      arrows: false,
+      asNavFor: bigSlider,
+      autoplay: false,
+      adaptiveHeight: false,
+      slidesToShow: 4,
+      slidesToScroll: 1,
+      infinite: false,
+    });
+  }
 });
